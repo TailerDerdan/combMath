a57933c1b8c9f80a9e027c9c5d7f3448d37d4ade
TailerDerdan <105823298+TailerDerdan@users.noreply.github.com>
2026-02-04 22:57:22 +0300

    добавил gitignore, уточнил readme, добавил 1.1 задание

---
+++ labs/Salesman/Salesman.cpp
@@ -0,0 +1,30 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+using Num = long;
+
+using matrix = std::vector<std::vector<Num>>;
+
+void GetSumOfEdges(const matrix& mat, const std::vector<int>& setVertex)
+{
+    Num sum = 0;
+    for (size_t iter = 1; iter < setVertex.size(); iter++)
+    {
+        sum += mat[iter][];
+    }
+}
+
+int main()
+{
+    std::vector<int> v = { -6, -5, -4, -3, -2, -1, 0 };
+    auto start = std::chrono::high_resolution_clock::now();
+
+    
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+
+    std::cout << duration.count() << std::endl;
+}
