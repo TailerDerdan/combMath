9a477ae37304a4d24bb7975847062ac9dcf83269
TailerDerdan <105823298+TailerDerdan@users.noreply.github.com>
2026-02-04 23:51:32 +0300

    сделал задачу

--- labs/Salesman/Salesman.cpp
+++ labs/Salesman/Salesman.cpp
@@ -2,29 +2,145 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <optional>
+#include <fstream>
+#include <regex>
+#include <numeric>
 
 using Num = long;
 
 using matrix = std::vector<std::vector<Num>>;
 
-void GetSumOfEdges(const matrix& mat, const std::vector<int>& setVertex)
+struct Args
 {
-    Num sum = 0;
-    for (size_t iter = 1; iter < setVertex.size(); iter++)
-    {
-        sum += mat[iter][];
-    }
+    std::string input;
+    std::string output;
+};
+
+std::optional<Args> ParseArgs(int argc, char* argv[])
+{
+	std::string input;
+	std::string output;
+
+	Args args;
+
+	if (argc != 3)
+	{
+		std::cout << "Invalid argument count\n"
+			<< "Usage: salesman.exe <inputFile> <outputFile>\n";
+		return std::nullopt;
+	}
+
+	args.input = argv[1];
+	args.output = argv[2];
+	return args;
 }
 
-int main()
+std::vector<Num> GetVectorFromStr(const std::string& row)
 {
-    std::vector<int> v = { -6, -5, -4, -3, -2, -1, 0 };
+	std::regex exp(R"(\s+)");
+	std::vector<std::string> tokens(
+		std::sregex_token_iterator(row.begin(), row.end(), exp, -1),
+		std::sregex_token_iterator()
+	);
+
+	tokens.erase(
+		std::remove_if(tokens.begin(), tokens.end(),
+			[](const std::string& s) { return s.empty(); }),
+		tokens.end()
+	);
+	
+	std::vector<Num> rowNums;
+	for (auto token : tokens)
+	{
+		Num num = std::stol(token);
+		rowNums.push_back(num);
+	}
+	return rowNums;
+}
+
+matrix GetMatrixFromFile(std::istream& inputFile)
+{
+	std::string row;
+	matrix result;
+
+	while (std::getline(inputFile, row))
+	{
+		result.push_back(GetVectorFromStr(row));
+	}
+	return result;
+}
+
+void PrintMatrix(std::ostream& out, const matrix& mat)
+{
+	for (size_t iterCol = 0; iterCol < mat.size(); iterCol++)
+	{
+		for (size_t iterRow = 0; iterRow < mat[iterCol].size(); iterRow++)
+		{
+			out << mat[iterCol][iterRow] << "	";
+		}
+		out << std::endl;
+	}
+}
+
+Num GetSumOfEdges(const matrix& mat, const std::vector<size_t>& setVertex)
+{
+	Num sum = 0;
+	for (size_t iter = 1; iter < setVertex.size(); iter++)
+	{
+		sum += mat[setVertex[iter - 1]][setVertex[iter]];
+	}
+	sum += mat[setVertex[setVertex.size() - 1]][setVertex[0]];
+	return sum;
+}
+
+std::vector<size_t> GetShortestPath(const matrix& mat)
+{
+	std::vector<size_t> setOfVertex(mat.size());
+	std::iota(setOfVertex.begin(), setOfVertex.end(), size_t(0));
+
+	size_t resultSum = SIZE_MAX;
+	size_t curSum = 0;
+	std::vector<size_t> vertexShortestPath;
+
+	do
+	{
+		curSum = GetSumOfEdges(mat, setOfVertex);
+		if (curSum < resultSum)
+		{
+			resultSum = curSum;
+			vertexShortestPath = setOfVertex;
+		}
+		
+	} while (std::next_permutation(setOfVertex.begin(), setOfVertex.end()));
+
+	return vertexShortestPath;
+}
+
+int main(int argc, char* argv[])
+{
+	auto args = ParseArgs(argc, argv);
+	if (!args)
+	{
+		return EXIT_FAILURE;
+	}
+
+	std::ifstream inputFile;
+	std::ofstream outputFile;
+	inputFile.open(args.value().input);
+	outputFile.open(args.value().output);
+
+	auto matrix = GetMatrixFromFile(inputFile);
+
     auto start = std::chrono::high_resolution_clock::now();
 
-    
+	auto shortestPath = GetShortestPath(matrix);
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
 
     std::cout << duration.count() << std::endl;
+
+	copy(shortestPath.begin(), shortestPath.end(), std::ostream_iterator<size_t>(outputFile, " "));
+	outputFile << std::endl << GetSumOfEdges(matrix, shortestPath);
 }
